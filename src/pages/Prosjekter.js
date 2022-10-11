a4ed2610a49b5dc62b4526820e513c3a095c204c
OleKodehode <98528472+OleKodehode@users.noreply.github.com>
2022-10-11 15:16:32 +0200

    Update Prosjekter.js

--- src/pages/Prosjekter.js
+++ src/pages/Prosjekter.js
@@ -1,26 +1,49 @@
-import { Header, Tittel } from "../globalComponents/NavBar";
+import { 
+    Header, 
+    Tittel 
+} from "../globalComponents/NavBar";
 import { UnderTittel } from "./landingPage/LandingPageComp";
-import { FlexLeft, FlexRight, KontaktText, ParallaxPreview, PokeDexPreview } from "./prosjekterComp/ProsjekterComp";
+import { 
+    FlexLeft, 
+    FlexRight, 
+    KontaktText, 
+    ParallaxPreview, 
+    PokeDexPreview 
+} from "./prosjekterComp/ProsjekterComp";
 
 
 
 function Prosjekter() {
     return(
         <>
-            <Tittel>Mine prosjekter</Tittel>
+            <Tittel>
+                Mine prosjekter
+            </Tittel>
             <FlexRight>
-                <a href="https://olekodehode.github.io/pokedex/" target="_blank" rel="noreferrer">
-                <PokeDexPreview />        
+                <a 
+                    href="https://olekodehode.github.io/pokedex/" 
+                    target="_blank" rel="noreferrer"
+                >
+                    <PokeDexPreview />        
                 </a>
-                <KontaktText>I dette prosjektet har brukt vanilla javascript der jeg laget en pokedex ved hjelp av et API call, som går igjennom alle de 898 Pokémonene, og viser forskjellig type informasjon jeg har hentet ut.</KontaktText>        
+                <KontaktText>
+                    I dette prosjektet har brukt vanilla javascript der jeg laget en pokedex ved hjelp av et API call, som går igjennom alle de 898 Pokémonene, og viser forskjellig type informasjon jeg har hentet ut.
+                </KontaktText>        
             </FlexRight>
             <FlexLeft>
-                <a href="https://olekodehode.github.io/parallax/" target="_blank" rel="noreferrer">
+                <a 
+                    href="https://olekodehode.github.io/parallax/" 
+                    target="_blank" rel="noreferrer"
+                >
                 <ParallaxPreview />
                 </a>
-                <KontaktText>I dette prosjektet laget jeg en utrolig fin parallax nettside som er opptimalisert for alle skjermstørrelser og lærte mye om styling på bilder.</KontaktText>
+                <KontaktText>
+                    I dette prosjektet laget jeg en utrolig fin parallax nettside som er opptimalisert for alle skjermstørrelser og lærte mye om styling på bilder.
+                </KontaktText>
             </FlexLeft>
-            <UnderTittel>Siden er under oppbygging</UnderTittel>
+            <UnderTittel>
+                Siden er under oppbygging
+            </UnderTittel>
         </>
         
     )
